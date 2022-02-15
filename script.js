cd3322c15db9fc85ce6bc93253f51a9aa0c5c398
Fran Ribeiro <ribeirofrv@gmail.com>
2022-02-15 20:32:22 -0300

    Cumpre Requisito 4 & 5 - Cria Quadro de Pixels e Define Seu Tamanho Padrao

---
+++ script.js
@@ -0,0 +1,13 @@
+const pixelBoard = document.getElementById('pixel-board'); // seleciona o quadro
+
+const createPixelBoard = (size) => {
+  for (let line = 0; line < size; line += 1) {
+    const pixelsLine = document.createElement('div');
+    pixelBoard.appendChild(pixelsLine).classList.add('pixel');
+    pixelsLine.classList.add('standardSize');// cria pixels
+  }
+};
+
+window.onload = () => {
+  createPixelBoard(25);
+};
